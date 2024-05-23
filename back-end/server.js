d03c7edb7dd816bdb1346a80113091d344c194b4
Tan <tan10tvo@gmail.com>
2024-05-23 11:41:07 -0600

    index

--- back-end/server.js
+++ back-end/server.js
@@ -4,6 +4,8 @@ import 'dotenv/config'
 
 import mongoConfig from './config.js'
 
+import Todo from './models/TodoModel.js'
+
 const app = express()
 
 const port = 8080
@@ -11,6 +13,17 @@ const port = 8080
 app.get('/', (req, res) =>{
     res.json('Hello! (from Server)')
 } )
+
+app.get('/api/todos', async (req, res) => {
+    try{
+        const todos = await Todo.find()
+        res.status(200).json(todos)
+
+    } catch (err){
+        console.log(err.message)
+        res.status(400).json(err)
+    }
+})
 
 app.listen(port, () => {
     console.log('listening on port: ' + port)
